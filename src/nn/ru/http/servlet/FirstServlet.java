5cb445a31b86500b60c39fdcc45649d2636e6ce0
voyagerdva <mariner-9@yandex.ru>
2023-12-22 14:46:13 +0300

    postman

---
+++ src/nn/ru/http/servlet/FirstServlet.java
@@ -0,0 +1,52 @@
+package nn.ru.http.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.Map;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var paramValue = req.getParameter("param");
+        var parameterMap = req.getParameterMap();
+        System.out.println();
+
+        var headerNames = req.getHeaderNames();
+        while ((headerNames.hasMoreElements())) {
+            var header = headerNames.nextElement();
+        }
+
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setHeader("token", "12345");
+//        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        try (var writer = resp.getWriter()) {
+            writer.write("<h1>Привет с первого сервлета! </h1>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var parameterMap = req.getParameterMap();
+        System.out.println();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
